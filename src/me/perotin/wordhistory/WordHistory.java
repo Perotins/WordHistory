d4f58df957c89cb54d47134f48f79e12b10c32bb
Perotin <maxfuligni@gmail.com>
2017-11-14 23:20:21 -0500

    created WordFile system

--- src/me/perotin/wordhistory/WordHistory.java
+++ src/me/perotin/wordhistory/WordHistory.java
@@ -7,9 +7,16 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class WordHistory extends JavaPlugin{
 
+    /// Singleton for making WordFile life easier
+    private static WordHistory plugin;
+
     @Override
     public void onEnable(){
+        plugin = this;
+    }
 
+    public static WordHistory getPlugin (){
+        return plugin;
     }
 
 }
--- src/me/perotin/wordhistory/files/WordFile.java
+++ src/me/perotin/wordhistory/files/WordFile.java
@@ -1,6 +1,60 @@
 package me.perotin.wordhistory.files;
 
+import me.perotin.wordhistory.WordHistory;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
 public class WordFile {
+
+
+    private File file;
+    private FileConfiguration configuration;
+
+    public WordFile(WordFileType type){
+        // hard-coding some stuff to make our lives easier, since we'll be using
+        // files A LOT so it is important we have a good system set in place to easily retreive these things
+
+        if(type == WordFileType.MESSAGES.MESSAGES){
+            file = new File(WordHistory.getPlugin().getDataFolder(), "messages.yml");
+            configuration = YamlConfiguration.loadConfiguration(file);
+        }
+        if(type == WordFileType.PLAYERS){
+            file = new File(WordHistory.getPlugin().getDataFolder(), "players.yml");
+            configuration = YamlConfiguration.loadConfiguration(file);
+        }
+
+
+
+
+
+    }
+
+    // some generic methods to speed up the process
+
+    public void save(){
+        try{
+            configuration.save(file);
+        } catch (IOException ex){
+            ex.printStackTrace();;
+        }
+    }
+
+
+    public FileConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public Object get(String path){
+        return configuration.get(path);
+    }
+
+    public void set(String path, Object value){
+        configuration.set(path, value);
+    }
+
 
 
 }
